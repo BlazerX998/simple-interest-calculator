5aa553236b0156a65cdf055d7ebeed68f4d22a9a
Prince Johnson <65087254+BlazerX998@users.noreply.github.com>
2026-04-01 21:40:20 +0530

    Add simple interest calculation script

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+# simple-interest.sh 
+# Author: Prince Johnson
 
+
+echo "Enter Principal:"
+read p
+echo "Enter Rate of Interest:"
+read r
+echo "Enter Time (in years):"
+read t
+
+si=$(echo "scale=2; $p * $r * $t / 100" | bc)
+echo "Simple Interest = $si"
